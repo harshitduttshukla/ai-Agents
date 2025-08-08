bf1ccd387d5246e29a097e21149b2f6fe2123036
Harshit Shukla <harshitshukla6388@gmail.com>
2025-08-08 12:15:59 +0530

    add the updateuser and getuers api

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,5 @@
 import bcrypt  from "bcrypt"
-import jwt, { decode } from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 import User from "../models/user"
 import { inngest } from "../inngest/client";
 
@@ -69,5 +69,42 @@ export const logout = async (req,res) => {
         res.json({message : "Logout successfully"});
     } catch (error) {
         res.status(500).json({error : "Login failed", details : erorr.message});
+    }  
+}
+
+export const updateUser = async (req,res) => {
+    const {skills = [],role,email} = req.body;
+    try {
+
+        if(req.user?.role !== "admin"){
+            return res.status(403).json({ error : "Forbidden"});
+        }
+        const user = await User.findOne({email});
+        if(!user){
+            return res.status(401).json({error : "User not found"});
+
+        }
+
+        await User.findOne(
+            {email},
+        {skills : skills.length ? skills : user.skills, role}
+        );
+        return res.json({message : "User update successfully"});
+    } catch (error) {
+        res.status(500).json({error : "Update failed",details : error.message})
+    }
+}
+
+export const getUsers = async ( req, res) => {
+    try {
+        if(req.user.role !== "admin"){
+            return res.status(403).json({error : "Forbidden"});
+
+        }
+        const users = await User.find().select("-password");
+        return res.json(users);
+        
+    } catch (error) {
+        res.status(500).json({erorr : 'Update failed', details:error.message});
     }
 }
